1d6b892a6cff077074ecd460e23834d15de10f43
AvivAbachi <AvivAbachi@Gmail.com>
2021-07-16 13:23:08 +0300

    clean main.jsx

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -4,14 +4,8 @@ import './scss/style.scss';
 import { AppContextProvider } from './hooks';
 
 ReactDOM.render(
-	// <UserProvider>
-	// 	<ModalProvider>
-	// 		<PostProvider>
 	<AppContextProvider>
 		<App />
 	</AppContextProvider>,
-	// 		</PostProvider>
-	// 	</ModalProvider>
-	// </UserProvider>
 	document.getElementById('root')
 );
