a53ce0267e24680d46802280242801d172d4bf14
aviv <avivabachi@gmail.com>
2022-01-26 00:13:24 +0200

    Zustand Improve And Clean

--- web/src/hooks/index.js
+++
@@ -1,5 +0,0 @@
-import UserProvider, { UserContext } from './useUser';
-import ModalProvider, { ModalContext } from './useModal';
-import PostProvider, { PostContext } from './usePost';
-
-export { UserProvider, PostProvider, ModalProvider, UserContext, ModalContext, PostContext };
--- web/src/store/index.js
+++ web/src/store/index.js
@@ -1,5 +1,5 @@
 import create from 'zustand';
-import { devtools } from 'zustand/middleware';
+import { devtools, persist } from 'zustand/middleware';
 import { mountStoreDevtool } from 'simple-zustand-devtools';
 
 import userSlice from './user';
@@ -7,40 +7,21 @@ import postSlice from './post';
 import modalSlice from './modal';
 
 export const useStore = create(
-	devtools((set, get) => ({
-		...userSlice(set, get),
-		...postSlice(set, get),
-		...modalSlice(set, get),
-	}))
+	devtools(
+		persist(
+			(set, get) => ({
+				...userSlice(set, get),
+				...postSlice(set, get),
+				...modalSlice(set, get),
+			}),
+			{
+				name: 'setting',
+				partialize: (store) => ({ token: store.token }),
+				getStorage: () => sessionStorage,
+			}
+		),
+		{ serialize: { options: true } }
+	)
 );
-
-export const selector = {
-	//User
-	user: (state) => state.user,
-	load: (state) => state.load,
-	token: (state) => state.token,
-	signup: (state) => state.signup,
-	login: (state) => state.login,
-	logout: (state) => state.logout,
-	getAccess: (state) => state.getAccess,
-	update: (state) => state.update,
-	reset: (state) => state.reset,
-	//Post
-	post: (state) => state.post,
-	userPost: (state) => state.userPost,
-	serverError: (state) => state.serverError,
-	getPost: (state) => state.getPost,
-	toggleUserPost: (state) => state.toggleUserPost,
-	createPost: (state) => state.createPost,
-	updatePost: (state) => state.updatePost,
-	deletePost: (state) => state.deletePost,
-	reloadPost: (state) => state.reloadPost,
-	//Modal
-	modal: (state) => state.modal,
-	type: (state) => state.modal.type,
-	handelModal: (state) => state.handelModal,
-	resetPassword: (state) => state.resetPassword,
-	resetSuccess: (state) => state.resetSuccess,
-};
 
 export default useStore;
