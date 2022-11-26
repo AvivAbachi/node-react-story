8583bbe02c850d07a4e009c1c3f0238d08359832
aviv <avivabachi@gmail.com>
2022-11-26 23:43:19 +0200

    ESLint

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,7 +1,12 @@
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './scss/style.scss';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
-root.render(<React.StrictMode children={<App />} />);
+root.render(
+	<StrictMode>
+		<App />
+	</StrictMode>
+);
