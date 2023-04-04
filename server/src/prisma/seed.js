3cea2ea25d8328f67dda5a08e4451eb78fe695e6
Aviv Abachi <AvivAbachi@gmail.com>
2023-04-04 13:34:12 +0300

    improve db seeding

--- server/src/prisma/seed.js
+++ server/src/prisma/seed.js
@@ -1,31 +1,54 @@
-const { PrismaClient } = require('@prisma/client');
+import { PrismaClient } from '@prisma/client';
+import argon2 from 'argon2';
+
+import data from './data.json';
+
 const prisma = new PrismaClient();
-const argon2 = require('argon2');
-const data = require('./data.json');
 
-(async function () {
-	try {
-		console.log('Start seeding ...');
-		console.log('Seeding users...');
-		const users = data.users;
-		for (let i = 0; i < users.length; i++) {
-			users[i].password = await argon2.hash('ABCabc123');
-		}
-		await prisma.user.createMany({ data: users });
+async function seedDatabase() {
+	console.log('Start seeding...');
 
-		console.log('Seeding posts...');
-		const usersIdList = await prisma.user.findMany({ select: { userId: true } });
-		const posts = data.posts;
-		for (let i = 0; i < posts.length; i++) {
-			const j = Math.floor(usersIdList.length * Math.random());
-			posts[i].userId = usersIdList[j].userId;
-		}
-		await prisma.post.createMany({ data: posts });
-		console.log('Seeding finished.');
-	} catch (err) {
-		console.error(err);
-		process.exit(1);
-	} finally {
-		await prisma.$disconnect();
-	}
-})();
+	await seedUsers();
+	await seedPosts();
+
+	console.log('Seeding finished!');
+}
+
+async function seedUsers() {
+	console.log('Seeding users...');
+
+	const defaultPassword = 'ABCabc123';
+
+	const users = await Promise.all(
+		data.users.map(async (user) => {
+			const hashedPassword = await argon2.hash(defaultPassword);
+			return { ...user, password: hashedPassword };
+		})
+	);
+
+	await prisma.user.createMany({ data: users });
+}
+
+async function seedPosts() {
+	console.log('Seeding posts...');
+
+	const maxTimeDelta = 6 * 60 * 60 * 1000; // 6 hours in milliseconds
+	const date = new Date();
+	const users = await prisma.user.findMany({ select: { userId: true } });
+
+	const seededPosts = data.posts.map((post) => {
+		const userId = Math.floor(users.length * Math.random());
+		const createdAt = new Date(date);
+		const updatedAt = createdAt;
+
+		date.setTime(date.getTime() - Math.random() * maxTimeDelta);
+
+		return { ...post, userId, createdAt, updatedAt };
+	});
+
+	await prisma.post.createMany({ data: seededPosts });
+}
+
+seedDatabase().catch((error) => {
+	console.error(error);
+});
